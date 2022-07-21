1a3feaea0ae06d527a6681fc8a9d2bf610bb42ec
jasvin <jasvin@jasvin-PC>
2022-07-22 03:32:08 +0530

    added

--- spring-boot-jwt/src/main/java/com/javainuse/controller/HelloWorldController.java
+++ spring-boot-jwt/src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin()
 public class HelloWorldController {
-
+//http://localhost:9090/hello
 	@RequestMapping({ "/hello" })
 	public String hello() {
 		return "Hello World";
 	}
-	
+	//http://localhost:9090/jas
 	@RequestMapping({ "/jas" })
 	public String hello1() {
 		return "Hai jasvin";
